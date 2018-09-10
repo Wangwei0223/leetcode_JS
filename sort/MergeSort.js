167aa3ab3f8febdafc8e8902d7f553958250ac0f
Wangwei0223 <291978313@qq.com>
2018-09-10 15:32:37 -0400

    多个有序数组排序

--- sort/MergeSort.js
+++ sort/MergeSort.js
@@ -12,15 +12,34 @@ function MergeSort(arr) {
 function Merge(left, right) {
     var res = [];
     while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            res.push(left.shift());
-        }
-        else {
-            res.push(right.shift());
-        }
+        left[0] < right[0] ? res.push(left.shift()) : res.push(right.shift());
     }
     return res.concat(left, right);
 }
 
 var test_arr = [1, 3, 3, 7, 8, 3, 3, 5, 34, 65, 3, 4, 2, 3, 5, 6, 3, 3, 2, 5, 3]
 console.log(MergeSort(test_arr));
+
+/**
+ * 归并排序应用: 多个有序数组合并 
+ */
+
+function MergeArr(arrs) {
+    if (arrs.length === 1) return arrs[0];
+    var mid = Math.floor(arrs.length / 2);
+    var left = arrs.slice(0, mid);
+    var right = arrs.slice(mid);
+    return Merge_(MergeArr(left), MergeArr(right));
+}
+
+function Merge_(left, right) {
+    var i = 0, j = 0, res = [];
+    while (left.length > i && right.length > j) {
+        left[i] < right[j] ? res.push(left[i++]) : res.push(right[j++]);
+    }
+    return res.concat(left.slice(i), right.slice(j));
+}
+
+var test_arrs = [[1, 2, 3], [6, 7, 8], [9, 10, 11]];
+
+console.log(MergeArr(test_arrs));
