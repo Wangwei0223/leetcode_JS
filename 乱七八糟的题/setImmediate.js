e43390fc0312daa077910450fe02af72776db46b
Wangwei0223 <291978313@qq.com>
2018-09-26 21:59:21 -0400

    node event loop

--- 乱七八糟的题/setImmediate.js
+++ 乱七八糟的题/setImmediate.js
@@ -1,19 +1,58 @@
-//加入2个nextTick()的回调函数
-process.nextTick(function () {
-    console.log("nextTick延迟执行1");
-});
+// //加入2个nextTick()的回调函数
+// process.nextTick(function () {
+//     console.log("nextTick延迟执行1");
+// });
 
-process.nextTick(function () {
-    console.log("nextTick延迟执行2");
+// process.nextTick(function () {
+//     console.log("nextTick延迟执行2");
+// });
+// //加入两个setImmediate()回调函数
+// setImmediate(function () {
+//     console.log("setImmediate延迟执行1");
+//     process.nextTick(function () {
+//         console.log("强势插入");
+//     });
+// });
+// setImmediate(function () {
+//     console.log("setImmediate延迟执行2");
+// });
+// console.log("正常执行");
+
+//加入2个nextTick()的回调函数
+process.nextTick(function(){
+    console.log("nextTick延迟执行A");
 });
-//加入两个setImmediate()回调函数
-setImmediate(function () {
-    console.log("setImmediate延迟执行1");
-    process.nextTick(function () {
-        console.log("强势插入");
+process.nextTick(function(){
+    console.log("nextTick延迟执行B");
+    setImmediate(function(){
+        console.log("setImmediate延迟执行C");
+    });
+    process.nextTick(function(){
+        console.log("nextTick延迟执行D");
     });
 });
-setImmediate(function () {
-    console.log("setImmediate延迟执行2");
+
+
+//加入两个setImmediate()回调函数
+setImmediate(function(){
+    console.log("setImmediate延迟执行E");
+    process.nextTick(function(){
+        console.log("强势插入F");
+    });
+    setImmediate(function(){
+        console.log("setImmediate延迟执行G");
+    });
 });
-console.log("正常执行");
+setImmediate(function(){
+    console.log("setImmediate延迟执行H");
+    process.nextTick(function(){
+        console.log("强势插入I");
+    });
+    process.nextTick(function(){
+        console.log("强势插入J");
+    });
+    setImmediate(function(){
+        console.log("setImmediate延迟执行K");
+    });
+});
+console.log("正常执行L");
