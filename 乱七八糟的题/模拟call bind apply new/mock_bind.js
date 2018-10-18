1d1eca74c2f7a2421aa09d5c4e5b8700f3796196
Wangwei0223 <291978313@qq.com>
2018-10-18 19:18:27 -0400

    适用于new 的bind

--- 乱七八糟的题/模拟call bind apply new/mock_bind.js
+++ 乱七八糟的题/模拟call bind apply new/mock_bind.js
@@ -5,23 +5,27 @@ let obj = {
     name: 'wei wang'
 }
 
-function foo(){
+function foo() {
     console.log(this.name);
     console.log(arguments);
 }
 
-Function.prototype.bind_ = function($this){
+Function.prototype.bind_ = function ($this) {
     let argv = [];
-    for(let i = 1; i < arguments.length; i++){
+    for (let i = 1; i < arguments.length; i++) {
         argv.push(arguments[i]);
     }
     let this_ = this;
-    return function(){
-        for(let i = 0; i < arguments.length; i++){
+    let fNOP = function () {};
+    let bar = function () {
+        for (let i = 0; i < arguments.length; i++) {
             argv.push(arguments[i]);
         }
-        return this_.apply($this, argv);
+        return this_.apply(this instanceof fNOP ? this : $this, argv);
     }
+    fNOP.prototype = this.prototype;
+    bar.prototype = new fNOP();
+    return bar;
 }
 
 let newFunction = foo.bind_(obj, ...[1, 2, 3]);
@@ -32,4 +36,20 @@ newFunction(...[4, 5, 6]);
 /**
  * 一个绑定函数也能使用new操作符创建对象：这种行为就像把原函数当成构造器。提供的 this 值被忽略，同时调用时的参数被提供给模拟函数。
  */
+
+let obj_ = {
+    name: 'wei wang',
+    age: 23
+}
+
+function bar(value) {
+    console.log(this.name);
+    console.log(this.age);
+    this.value = 23;
+}
+let newCreation = bar.bind_(obj_, 23);
+
+let newObj_ = new newCreation(23);
+
+console.log(newObj_.value);
 
