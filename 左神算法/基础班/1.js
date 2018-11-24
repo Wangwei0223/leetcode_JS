f8224f5cf3d7da7666d9f9f0dbacc26705ea5b0d
Wangwei0223 <291978313@qq.com>
2018-11-23 23:25:31 -0500

    基础1

---
+++ 左神算法/基础班/1.js
@@ -0,0 +1,23 @@
+// A有序 B无序 打印B中所有不在A的数
+
+var A = [1, 2, 3, 4, 5, 6, 7, 8];
+var B = [6, 2, 6, 8, 9, 10, 12, 0];
+
+/**
+ * 直接indexOf
+ * @param {*} a 
+ * @param {*} b 
+ */
+function printExclude(a, b) { 
+    for(let i = 0; i < b.length; i++){
+        if (a.indexOf(b[i]) === -1){ // indexOf复杂度 ?
+            console.log(b[i]);
+        }
+    }
+}
+
+printExclude(A, B);
+
+function printExclude_(a, b){
+
+}
