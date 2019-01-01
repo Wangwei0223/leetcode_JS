e00d8b6d7983857dc1a50ac2db768c6add05ca94
Wangwei0223 <291978313@qq.com>
2019-01-01 08:07:22 -0500

    堆排序

--- 左神算法/基础班/2.js
+++ 左神算法/基础班/2.js
@@ -28,7 +28,7 @@ let arr = [1, 6, 5, 7, 5, 8, 3, 6, 5, 5, 2, 2, 3, 5, 9, 9];
 
 partition(arr, 5);
 
-console.log(arr);
+console.log('partition', arr);
 
 /**
  * 给定num, 小于num的放在num左边, 大于num的放在右边, 等于numd的放中间
@@ -61,10 +61,18 @@ let arr_1 = [1, 6, 5, 7, 5, 8, 3, 6, 5, 5, 2, 2, 3, 5, 9, 9];
 
 NetherlandFlag(arr_1, 5);
 
-console.log(arr_1);
+console.log('荷兰国旗', arr_1);
 
+/**
+ * 快排 写全了应该加上数组的判断
+ * @param {*} arr 
+ * @param {*} left 
+ * @param {*} right 
+ */
 function QuickSort(arr, left, right) {
     if (left < right) {
+        // 随机快排, 随机选一个数让它和最后一个交换, 随机快排 长期期望 O(n*logn)
+        swap(arr, left + parseInt(Math.random() * (right - left)), right - 1);
         let temp = QuickSortPartition(arr, left, right);
         QuickSort(arr, left, temp[0]);
         QuickSort(arr, temp[1], right);
@@ -72,6 +80,12 @@ function QuickSort(arr, left, right) {
 
 }
 
+/**
+ * 快排的partition
+ * @param {*} arr 
+ * @param {*} left 
+ * @param {*} right 
+ */
 function QuickSortPartition(arr, left, right) {
     let less = left - 1;
     let more = right;
@@ -91,4 +105,42 @@ function QuickSortPartition(arr, left, right) {
 
 let arr_2 = [1, 6, 5, 7, 5, 8, 3, 6, 5, 5, 2, 2, 3, 5, 9, 9];
 QuickSort(arr_2, 0, arr_2.length);
-console.log('快排: ', arr_2);
+console.log('快排', arr_2);
+
+/**
+ * 堆排序
+ * @param {*} arr 
+ */
+function HeapSort(arr) {
+    //构建堆
+    for (let i = Math.floor(arr.length / 2) - 1; i >= 0; i--) {
+        heapify(arr, i, arr.length);
+    }
+    console.log('构建完最大堆', arr);
+    //堆排序
+    let len = arr.length;
+    for (let i = arr.length - 1; i > 0; i--) {
+        swap(arr, i, 0);
+        heapify(arr, 0, --len);
+    }
+    console.log('堆排序', arr);
+}
+
+function heapify(arr, index, len) {
+    let left = index * 2 + 1, right = index * 2 + 2, largest = index;
+    if (left < len && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < len && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest !== index) {
+        swap(arr, largest, index);
+        heapify(arr, largest, len);
+    }
+}
+
+let arr_3 = [1, 6, 5, 7, 5, 8, 3, 6, 5, 5, 2, 2, 3, 5, 9, 9];
+
+HeapSort(arr_3);
+
