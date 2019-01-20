3521f05f33d131406de7521636064ee36c910ea8
Wangwei0223 <291978313@qq.com>
2019-01-19 22:52:15 -0500

    平衡二叉树

--- tree/110.js
+++ tree/110.js
@@ -22,3 +22,26 @@ var maxDepth = function(root){
     if(!root) return 0;
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * Leetcode 110 平衡二叉树
+ */
+var isBalanced = function(root) {
+    if(!root) return true;
+    if(Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) return false;
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+
+var maxDepth = function(root){
+    if(!root) return 0;
+    return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+}
