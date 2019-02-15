8a055279eae4e820e79985580cdcc4c60040da50
Wangwei0223 <291978313@qq.com>
2019-02-15 00:20:50 -0500

    add 1

---
+++ tree/test.js
@@ -0,0 +1,16 @@
+var a = [1, 2, 3, 4, null, 5];
+console.log(a.indexOf(5));
+
+
+let res = [];
+
+res.push(...[1, 2, 1, 2]);
+
+console.log(res);
+
+console.log(parseInt(1 / 2));
+
+
+let res1 = [[1, 2, 3, 4], [2, 3, 4, 5], [2, 4, 5, 6]];
+
+console.log(res1.indexOf(3));
