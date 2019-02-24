ae1dd989864710a6dde476cd45abc6f9d80c966f
Wangwei0223 <291978313@qq.com>
2019-02-23 23:49:29 -0500

    add 998

---
+++ tree/998.js
@@ -0,0 +1,23 @@
+/**
+ * 998. Maximum Binary Tree II
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+var insertIntoMaxTree = function(root, val) {
+    if(root !== null && root.val > val){
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+    let node = new TreeNode(val);
+    node.left = root;
+    return node;
+};
