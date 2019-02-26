bc70fcb9a8f253c0295ee9ca4a49b5acfb184623
Wangwei0223 <291978313@qq.com>
2019-02-25 23:34:51 -0500

    add LRU

--- 左神算法/进阶班/5.js
+++ 左神算法/进阶班/5.js
@@ -104,3 +104,121 @@ var depth = function(root){
     ans = Math.max(ans, left + right + 1);
     return Math.max(left, right) + 1;
 }
+
+/**
+ * 146. LRU Cache
+ */
+
+class NodeList{
+    contructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    
+    remove(){
+        this.head = this.head.next;
+        this.head.last.next = null;
+        this.head.last = null;
+    }
+    
+    add(node){
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        node.last = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
+    
+    move(node){
+        if(node === this.tail) return;
+        if(node === this.head){ // 记得动head
+            node.next.last = null;
+            this.head = node.next;
+            node.last = this.tail;
+            this.tail.next = node;
+            node.next = null;
+            this.tail = node;
+            return;
+        }
+        node.last.next = node.next; // 是head的时候node.last.next没有, 会报错
+        node.next.last = node.last;
+        node.last = this.tail;
+        this.tail.next = node;
+        node.next = null;
+        this.tail = node;
+    }
+    
+    returnHead(){
+        return this.head;
+    }
+}
+
+// A <-> B <-> C <-> D
+
+class Node{
+    constructor(key, val){
+        this.key = key;
+        this.val = val;
+        this.last = null;
+        this.next = null;
+    }
+}
+
+class LRUCache{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.KtoV = new Map();
+        this.VtoK = new Map();
+        this.nodeList = new NodeList(capacity);
+    }
+    
+    put(key, val){
+        if(this.KtoV.has(key)){
+            let node = this.KtoV.get(key);
+            node.val = val;
+            this.nodeList.move(node);
+            return;
+        }
+        let node = new Node(key, val);
+        this.KtoV.set(key, node);
+        this.VtoK.set(node, key);
+        this.nodeList.add(node);
+        this.capacity--;
+        if(this.capacity < 0){
+            let curHead = this.nodeList.returnHead();
+            this.KtoV.delete(this.VtoK.get(curHead));
+            this.VtoK.delete(curHead);
+            this.capacity++;
+            this.nodeList.remove();
+        }
+    }
+    
+    get(key){
+        if(!this.KtoV.has(key)){
+            console.log(-1);
+            return;
+        }
+        let node = this.KtoV.get(key);
+        this.nodeList.move(node);
+        console.log(node.val);
+        return node.val;
+    }
+
+    print(){
+        console.log(this.KtoV);
+        console.log('head:', this.nodeList.head);
+        console.log('last:', this.nodeList.tail);
+    }
+}
+
+let cache = new LRUCache(2);
+
+cache.put(2, 1);
+cache.put(1, 1);  
+cache.put(2, 3);    
+cache.put(4, 1);
+cache.get(1);
+cache.get(2);
