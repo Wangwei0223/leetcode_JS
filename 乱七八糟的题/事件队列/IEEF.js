e5f2c3b758107ec67c98426b9db2e5b237111486
Wangwei0223 <291978313@qq.com>
2019-03-26 23:25:26 -0400

    rname

--- 乱七八糟的题/事件队列/IEEF.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i < 10; i++) {
-    (function (i) {
-        setTimeout(function () {
-            console.log(i);
-        }, 10);
-    })(i);
-}
--- 乱七八糟的题/变量对象VO/VO2.js
+++
@@ -1,3 +0,0 @@
-var ab = require('./VO'); // require的时候就会执行 VO.js
-
-console.log(global.a);
