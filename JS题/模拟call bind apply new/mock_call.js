a1f8c0f3a559a9535fd35da9fbc7e59f31b1cd26
Wangwei0223 <291978313@qq.com>
2019-03-30 22:06:07 -0400

    复习mock_call

--- JS题/模拟call bind apply new/mock_call.js
+++ JS题/模拟call bind apply new/mock_call.js
@@ -1,3 +1,6 @@
+// https://github.com/mqyqingfeng/Blog/issues/11
+// JavaScript深入之call和apply的模拟实现
+
 var name = 'wei wang';
 
 function foo(){
@@ -26,12 +29,14 @@ foo.call(obj, ...[1, 2]);
 
 // 第三步 记得this为null 默认是window node环境就先写成global代替
 Function.prototype.call_ = function($this){
+    // this 可以获取调用call的函数
     $this.fn = this || global;
     // let args = Array.prototype.slice.call(arguments, 1); // 斜率arguments 跳过V8 优化
     let args = [];
     for(let i = 1;  i < arguments.length; i++){
         args.push(arguments[i]);
     }
+    // 不使用es6的话, let result = eval('context.fn(' + args + ')');
     let result = $this.fn(...args);
     delete $this.fn;
     return result;
