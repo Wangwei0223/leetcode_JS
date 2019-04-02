3e4b5960b2b4c4aec48ae76ddb6d4c5802f519e3
Wangwei0223 <291978313@qq.com>
2019-04-01 21:57:23 -0400

    模拟bind, 有点难度

--- JS题/模拟call bind apply new/mock_bind.js
+++ JS题/模拟call bind apply new/mock_bind.js
@@ -1,5 +1,6 @@
 // 代码摘抄自
 // https://github.com/mqyqingfeng/Blog/issues/12
+// JavaScript深入之bind的模拟实现
 
 let obj = {
     name: 'wei wang'
@@ -10,14 +11,40 @@ function foo() {
     console.log(arguments);
 }
 
+/**
+ * 要着重考虑bind传参的问题
+    * var foo = {
+        value: 1
+    };
+
+    function bar(name, age) {
+        console.log(this.value);
+        console.log(name);
+        console.log(age);
+
+    }
+
+    var bindFoo = bar.bind(foo, 'daisy');
+    bindFoo('18'); // 旧参数 + 新参数
+    // 1
+    // daisy
+    // 18
+ */
+
 Function.prototype.bind_ = function ($this) {
     let argv = [];
     for (let i = 1; i < arguments.length; i++) {
         argv.push(arguments[i]);
     }
     let this_ = this;
+
+    // 难点 一个绑定函数也能使用new操作符创建对象：
+    // 这种行为就像把原函数当成构造器。
+    // 提供的 this 值被忽略，同时调用时的参数被提供给模拟函数
+
     let fNOP = function () {};
     let bar = function () {
+        // 这个时候的arguments是指bind返回的函数传入的参数
         for (let i = 0; i < arguments.length; i++) {
             argv.push(arguments[i]);
         }
