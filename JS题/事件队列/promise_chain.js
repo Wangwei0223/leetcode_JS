d67ce80f1db864eb850c7f8d8672f5041777d307
Wangwei0223 <291978313@qq.com>
2019-04-15 22:44:54 -0400

    update promise

--- JS题/事件队列/promise_chain.js
+++ JS题/事件队列/promise_chain.js
@@ -20,9 +20,12 @@ var aPromise = new Promise(function (resolve) {
     resolve(100);
 });
 
+// 第二个then能不能获取第一个then的error, 看你怎么抛了
 aPromise.then(function (value) {
     console.log('0:' + value);
     return value * 2;
+}).then(function(value){
+    console.log('二级then', value);
 });
 aPromise.then(function (value) {
     console.log('2:' + value);
@@ -30,9 +33,12 @@ aPromise.then(function (value) {
 });
 aPromise.then(function (value) {
     console.log("1: " + value); // => 100
-    return Promise.reject(new Error('than error'));
+    // throw new Error('new err');
+    return Promise.reject(new Error('then error'));
 }).then(function(){
 
 }, function(err){
-    console.log('catched err:' + er);
-});
+    console.log('catched err:' + err);
+}).catch(function (err) { 
+    console.log('new err', err);
+ });
