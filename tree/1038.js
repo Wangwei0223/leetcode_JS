59099a3307246eacfb8410d97eb2d9c1a2d01c9b
Wangwei0223 <291978313@qq.com>
2019-05-05 20:26:20 -0400

    add 1038 遍历两次版

---
+++ tree/1038.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * Binary Search Tree to Greater Sum Tree
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    if(!root) return root;
+    let node = root, stack = [];
+    let curSum = 0;
+    let sum = inorderTraversal(root);
+    while(stack.length > 0 || node){
+        if(node){
+            stack.push(node);
+            node = node.left;
+        }else{
+            node = stack.pop();
+            let temp = node.val;
+            node.val = sum - curSum;
+            curSum += temp;
+            node = node.right;
+        }
+        
+    }
+    
+    return root;
+};
+
+var inorderTraversal = function(root) {
+    if(!root) return [];
+    let node = root, stack = [];
+    let sum = 0;
+    while(stack.length > 0 || node){
+        if(node){
+            stack.push(node);
+            node = node.left;
+        }else{
+            node = stack.pop();
+            sum += node.val;
+            node = node.right;
+        }
+        
+    }
+    return sum;
+};
