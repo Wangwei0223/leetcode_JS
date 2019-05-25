d5346ac330fbab3852985d008c2e5ca59c98f134
Wangwei0223 <291978313@qq.com>
2019-05-24 23:33:19 -0400

    加注释

--- bfs/1030.js
+++ bfs/1030.js
@@ -21,7 +21,7 @@ var allCellsDistOrder = function(R, C, r0, c0) {
             let nc = cur[1] + i[1];
             if(nr >= 0 && nr < R && nc >= 0 && nc < C && !visit[nr][nc]){
                 queue.push([nr, nc]);
-                visit[nr][nc] = true;
+                visit[nr][nc] = true; // visit一定写在里面, 不然有可能加入多余的点. 1,0 向上 和 0,1向左 都是0,0 所以visit要在点第一次进队列就变为true, 防止多次加入
             }
         }
     }
